0b68d1f71a53f172338276fe3e3b88a692ee91a3
David Parsons <dave@daveparsons.net>
2021-10-09 15:35:29 +0100

    WIP for version 3.0.5 - do not need to run tools scripts as root/administrator

--- win-gettools.cmd
+++ win-gettools.cmd
@@ -4,12 +4,6 @@ echo Get macOS VMware Tools 3.0.5
 echo ===============================
 echo (c) Dave Parsons 2011-21
 
-net session >NUL 2>&1
-if %errorlevel% neq 0 (
-    echo Administrator privileges required!
-    exit
-)
-
 pushd %~dp0
 .\python-win-embed-amd64\python.exe gettools.py
 popd
