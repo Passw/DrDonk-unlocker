40016f8548ad058151df84ca2570ef0e7b42b380
David Parsons <dave@daveparsons.net>
2021-10-09 14:54:24 +0100

    WIP for version 3.0.5 - add tools as assets for downloading

---
+++ lnx-gettools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Get macOS VMware Tools 3.0.5"
+echo "==============================="
+echo "(c) Dave Parsons 2015-21"
+
+# Ensure we only use unmodified commands
+export PATH=/bin:/sbin:/usr/bin:/usr/sbin
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo Getting VMware Tools...
+./gettools.py
+
+echo Finished!
---
+++ win-gettools.cmd
@@ -0,0 +1,20 @@
+@echo off
+setlocal ENABLEEXTENSIONS
+echo Get macOS VMware Tools 3.0.5
+echo ===============================
+echo (c) Dave Parsons 2011-21
+
+net session >NUL 2>&1
+if %errorlevel% neq 0 (
+    echo Administrator privileges required!
+    exit
+)
+
+pushd %~dp0
+
+echo Getting VMware Tools...
+.\python-win-embed-amd64\python.exe gettools.py
+
+popd
+
+echo Finished!
